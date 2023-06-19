b8f962050d70dd8f796c0f9bec559ee305883689
JoomlaBoat <ivankomlev@gmail.com>
2023-06-19 15:17:53 -0500

    First commit

---
+++ customtablesword.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * CustomTablesWord Joomla! 3.x Native Component
+ * @author Ivan Komlev <support@joomlaboat.com>
+ * @link http://www.joomlaboat.com
+ * @copyright (C) 2018-2023 Ivan Komlev
+ * @license GNU/GPL
+ **/
+
+defined('_JEXEC') or die('Restricted access');
+
+class plgContentCustomTablesWord extends JPlugin
+{
+
+}
