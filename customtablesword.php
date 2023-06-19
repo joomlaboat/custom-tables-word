62a56c88292e29560c5399d98469b75221deaa14
JoomlaBoat <ivankomlev@gmail.com>
2023-06-19 16:22:40 -0500

    Version 1.0 ready and tested

--- customtablesword.php
+++ customtablesword.php
@@ -7,9 +7,55 @@
  * @license GNU/GPL
  **/
 
-defined('_JEXEC') or die('Restricted access');
+namespace CustomTables;
 
-class plgContentCustomTablesWord extends JPlugin
+// no direct access
+if (!defined('_JEXEC') and !defined('WPINC')) {
+    die('Restricted access');
+}
+
+use JoomlaBasicMisc;
+
+class Twig_PHPWord_Tags
 {
+    function setvalues(string $templateFile, array $values, ?string $saveAsFile = null)
+    {
+        //Generates a new .docx file and returns the link to that file if successful.
 
+        if ($templateFile == '') {
+
+            echo 'Template file name not specified.';
+            return null;
+        }
+
+        if ($templateFile[0] != '/' and $templateFile[0] != '\\')
+            $templateFile = JPATH_SITE . DIRECTORY_SEPARATOR . $templateFile;
+
+        if (!file_exists($templateFile)) {
+            echo 'File "' . $templateFile . '" not exists.';
+            return null;
+        }
+
+        if ($saveAsFile === null) {
+            $saveAsFile = JoomlaBasicMisc::suggest_TempFileName('docx');
+        } else {
+            if ($saveAsFile[0] != '/' and $saveAsFile[0] != '\\')
+                $saveAsFile = JPATH_SITE . DIRECTORY_SEPARATOR . $saveAsFile;
+
+            if (file_exists($saveAsFile)) {
+                echo 'File "' . $saveAsFile . '" already exists.';
+                return null;
+            }
+        }
+
+        require_once 'bootstrap.php';
+
+        $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($templateFile);
+        foreach ($values as $key => $item) {
+            $templateProcessor->setValue($key, $item);
+        }
+
+        $templateProcessor->saveAs($saveAsFile);
+        return $saveAsFile;
+    }
 }
