696fc4c995b0f33f8e405d5fcf556ab063a68f99
JoomlaBoat <ivankomlev@gmail.com>
2024-01-11 12:13:00 -0500

    Year changed

--- customtablesword.php
+++ customtablesword.php
@@ -3,7 +3,7 @@
  * CustomTablesWord Joomla! 3.x Native Component
  * @author Ivan Komlev <support@joomlaboat.com>
  * @link http://www.joomlaboat.com
- * @copyright (C) 2018-2023 Ivan Komlev
+ * @copyright (C) 2018-2024 Ivan Komlev
  * @license GNU/GPL
  **/
 
